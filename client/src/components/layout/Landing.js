8c769197d098022e414e48b4187ea3d00c9653bd
AverageDemo <average.demo@gmail.com>
2019-01-05 06:17:51 -0500

    started working on react

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+    render() {
+        return (
+            <main role="main" className="container flex-fill p-3">
+                <h1>Bugtracker</h1>
+            </main>
+        );
+    }
+}
+
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <Link className="navbar-brand text-white" to="/">
+                        Bugtracker
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">
+                                    Home
+                                </Link>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/register">
+                                    Sign Up
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">
+                                    Login
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="nav-scroller bg-white shadow-sm">
+                    <nav className="nav nav-underline">
+                        <Link className="nav-link" to="#">
+                            Placeholder
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
