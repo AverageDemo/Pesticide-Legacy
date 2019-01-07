5631d1b74f68376fbd131d85b7f94bebecdba656
AverageDemo <average.demo@gmail.com>
2019-01-07 06:35:18 -0500

    added basic 404 as a placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import NoMatch from "./components/layout/NoMatch";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -41,6 +43,7 @@ class App extends Component {
                             <Route exact path="/register" component={Register} />
                             <Route exact path="/login" component={Login} />
                             <Route exact path="/newIssue" component={NewIssue} />
+                            <Route component={NoMatch} />
                             <Footer />
                         </div>
                     </div>
---
+++ client/src/components/layout/NoMatch.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+    return (
+        <div>
+            <h1 className="text-center">404 - Page Not Found</h1>
+        </div>
+    );
+}
