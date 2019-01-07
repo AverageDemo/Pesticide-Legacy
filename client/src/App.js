79af9dfcbfdf5a03b6169e0f57cb955cdb10dadd
AverageDemo <average.demo@gmail.com>
2019-01-07 06:36:17 -0500

    added the switch to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -39,11 +39,13 @@ class App extends Component {
                     <div className="App">
                         <div className="d-flex flex-column content-box">
                             <Navbar />
-                            <Route exact path="/" component={Landing} />
-                            <Route exact path="/register" component={Register} />
-                            <Route exact path="/login" component={Login} />
-                            <Route exact path="/newIssue" component={NewIssue} />
-                            <Route component={NoMatch} />
+                            <Switch>
+                                <Route exact path="/" component={Landing} />
+                                <Route exact path="/register" component={Register} />
+                                <Route exact path="/login" component={Login} />
+                                <Route exact path="/newIssue" component={NewIssue} />
+                                <Route component={NoMatch} />
+                            </Switch>
                             <Footer />
                         </div>
                     </div>
