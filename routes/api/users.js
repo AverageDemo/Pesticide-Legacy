33358c48c3cb13b7dbab735452ae4a488e2869eb
AverageDemo <average.demo@gmail.com>
2019-01-19 02:07:55 -0500

    cleaning up more code

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,7 +17,7 @@ const User = require("../../models/User");
  * @desc    Register a new user
  * @access  Public
  */
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
 
     if (!isValid) {
@@ -27,46 +27,47 @@ router.post("/register", (req, res) => {
     const email = req.body.email;
     const username = req.body.username;
 
-    User.find({
-        $or: [
-            { email: { $regex: new RegExp("^" + email + "$", "i") } },
-            { username: { $regex: new RegExp("^" + username + "$", "i") } }
-        ]
-    })
-        .then(user => {
-            if (user) {
-                user.forEach(thisUser => {
-                    if (!thisUser.email.localeCompare(email))
-                        errors.email = "Email is already in use";
-                    if (!thisUser.username.localeCompare(username))
-                        errors.username = "Username is already in use";
-                });
-            }
+    try {
+        const user = await User.find({
+            $or: [
+                { email: { $regex: new RegExp("^" + email + "$", "i") } },
+                { username: { $regex: new RegExp("^" + username + "$", "i") } }
+            ]
+        });
 
-            if (!isEmpty(errors)) return res.status(400).json(errors);
-
-            const newUser = new User({
-                email,
-                username,
-                name: req.body.name,
-                password: req.body.password,
-                isVerified: true, // Remove this line when email verification is integrated fully.
-                veriToken: randString.generate(10)
+        if (user) {
+            user.forEach(thisUser => {
+                if (!thisUser.email.localeCompare(email)) errors.email = "Email is already in use";
+                if (!thisUser.username.localeCompare(username))
+                    errors.username = "Username is already in use";
             });
+        }
 
-            bcryptjs.genSalt(10, (err, salt) => {
-                bcryptjs.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
+        if (!isEmpty(errors)) return res.status(400).json(errors);
 
-                    return newUser
-                        .save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err));
-                });
+        const newUser = new User({
+            email,
+            username,
+            name: req.body.name,
+            password: req.body.password,
+            isVerified: true, // Remove this line when email verification is integrated fully.
+            veriToken: randString.generate(10)
+        });
+
+        bcryptjs.genSalt(10, (err, salt) => {
+            bcryptjs.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+
+                return newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
             });
-        })
-        .catch(err => console.log(err));
+        });
+    } catch (e) {
+        next(e);
+    }
 });
 
 /*
@@ -74,7 +75,7 @@ router.post("/register", (req, res) => {
  * @desc    Login user / return JWT
  * @access  Public
  */
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     const { errors, isValid } = validateLoginInput(req.body);
 
     if (!isValid) {
@@ -83,38 +84,42 @@ router.post("/login", (req, res) => {
 
     const password = req.body.password;
 
-    User.findOne({ username: { $regex: new RegExp("^" + req.body.username + "$", "i") } }).then(
-        user => {
-            if (!user) {
-                errors.username = "User not found";
-                return res.status(404).json(errors);
-            }
+    try {
+        const user = await User.findOne({
+            username: { $regex: new RegExp("^" + req.body.username + "$", "i") }
+        });
 
-            bcryptjs.compare(password, user.password).then(isMatch => {
-                if (isMatch) {
-                    if (user.isVerified) {
-                        const payload = {
-                            id: user.id,
-                            username: user.username
-                        };
+        if (!user) {
+            errors.username = "User not found";
+            return res.status(404).json(errors);
+        }
 
-                        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-                            res.json({
-                                success: true,
-                                token: "Bearer " + token
-                            });
+        bcryptjs.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                if (user.isVerified) {
+                    const payload = {
+                        id: user.id,
+                        username: user.username
+                    };
+
+                    jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
                         });
-                    } else {
-                        errors.username = "Please confirm your email!";
-                        res.status(400).json(errors);
-                    }
+                    });
                 } else {
-                    errors.password = "Incorrect password";
+                    errors.username = "Please confirm your email!";
                     res.status(400).json(errors);
                 }
-            });
-        }
-    );
+            } else {
+                errors.password = "Incorrect password";
+                res.status(400).json(errors);
+            }
+        });
+    } catch (e) {
+        next(e);
+    }
 });
 
 /*
@@ -122,17 +127,23 @@ router.post("/login", (req, res) => {
  * @desc    Activate a user
  * @access  Public
  */
-router.get("/activate/:token", (req, res) => {
+router.get("/activate/:token", async (req, res) => {
     const veriToken = req.params.token;
-    User.findOneAndUpdate({ veriToken }, { $set: { isVerified: true, veriToken: null } })
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({ msg: "Invalid token" });
-            }
 
-            res.json({ msg: "Account activated" });
-        })
-        .catch(err => console.log(err));
+    try {
+        const user = await User.findOneAndUpdate(
+            { veriToken },
+            { $set: { isVerified: true, veriToken: null } }
+        );
+
+        if (!user) {
+            return res.status(404).json({ msg: "Invalid token" });
+        }
+
+        res.json({ msg: "Account activated" });
+    } catch (e) {
+        next(e);
+    }
 });
 
 /*
